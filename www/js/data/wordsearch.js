b9396cbe390c872c61055015d55b26588e6a4a06
Harpreet Singh <mrharpreets@gmail.com>
2015-10-18 18:19:42 +0530

    Updated cordova version

--- www/js/data/wordsearch.js
+++ www/js/data/wordsearch.js
@@ -48,137 +48,460 @@ var gridl1v7 = {
 	cols : 9
 }
 var gridl1v8 = {
-		words : "A;jI;q,ju;Jw;r,zo;rw;v;r,P;iq;h",
-		wordsToDisplay : null,
-		size : 6,
-		rows : 6,
-		cols : 9
-	}
+	words : "A;jI;q,ju;Jw;r,zo;rw;v;r,P;iq;h",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 9
+}
 
-var gridl2v1 = {
+var gridl1v9 = {
 	words : "ky;s,k;Vw,kM;Gw,ik;r;pw;n,k;iC;h;rw",
 	wordsToDisplay : null,
 	size : 6,
 	rows : 6,
 	cols : 9
 }
-var gridl2v2 = {
+var gridl1v10 = {
 	words : "Drm,ig;Aw;n,srm,krm,sc",
 	wordsToDisplay : null,
 	size : 6,
 	rows : 6,
 	cols : 9
 }
-var gridl2v3 = {
+var gridl1v11 = {
 	words : "d;ie;Aw,Drm,ihM;m;q,mo;h;k;m,sw;ih;b",
 	wordsToDisplay : null,
 	size : 6,
 	rows : 6,
 	cols : 9
 } 
-var gridl2v4 = {
+var gridl1v12 = {
 	words : "AY;q,so;m,mM;g;l,bu;D,vI;r,Su;k;r,S;nI",
 	wordsToDisplay : null,
 	size : 6,
 	rows : 6,
 	cols : 9
 }
-var gridl2v5 = {
+var gridl1v13 = {
 	words : "b;k;rI,mgrmC,ko;ie;l,b;gu;lw,hM;s,iC;p;k;lI",
 	wordsToDisplay : null,
 	size : 6,
 	rows : 6,
 	cols : 9
 }
-var gridl2v6 = {
+var gridl1v14 = {
 	words : "B;ro;sw,ip;Aw;r,s~;cw;eI,im;h;r;bw;nI,eI;mw;n,p;r;au;p;kw;r",
 	wordsToDisplay : null,
 	size : 6,
 	rows : 6,
 	cols : 9
 }
-var gridl2v7 = {
-		words : "iv;S;vw;s,B;lw;eI,sy;vw,s;m;J;dw;rI,sM;qo;K,DI;r;j",
-		wordsToDisplay : null,
-		size : 6,
-		rows : 6,
-		cols : 9
-	}
-var gridl2v8 = {
-		words : "d;ie;Aw,ib;by;k,iv;sw;h,im;T;bo;l;Vw,mdd,r;ih;q",
-		wordsToDisplay : null,
-		size : 6,
-		rows : 6,
-		cols : 9
-	}
+var gridl1v15 = {
+	words : "iv;S;vw;s,B;lw;eI,sy;vw,s;m;J;dw;rI,sM;qo;K,DI;r;j",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 9
+}
+var gridl1v16 = {
+	words : "d;ie;Aw,ib;by;k,iv;sw;h,im;T;bo;l;Vw,mdd,r;ih;q",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 9
+}
 
-var gridl3v1 = {
+var gridl2v1 = {
 	words : "kI;r;q;n,k;Vw,d;s;qw;r,ik;r;pw,AM;imR;q,Sbd,is;m;r;n",
 	wordsToDisplay : null,
 	size : 6,
 	rows : 6,
 	cols : 9
 }
-var gridl3v2 = {
+var gridl2v2 = {
 	words : "is;rI;rw;gu,mw;J,g;au;VI,Aw;sw,gU;j;rI,dy;v;gM;Dw;rI,ib;hw;g;Vw",
 	wordsToDisplay : null,
 	size : 6,
 	rows : 6,
 	cols : 9
 }
-var gridl3v3 = {
+var gridl2v3 = {
 	words : "v;f;hM;su,so;r;iT,D;nw;s;rI,jY;q;s;rI,to;fI,bY;rw;VI,iq;lM;g",
 	wordsToDisplay : null,
 	size : 6,
 	rows : 6,
 	cols : 9
 }
-var gridl3v4 = {
+var gridl2v4 = {
 	words : "sU;hI,ib;lw;v;lu,go M;f,rw;m;k;lI,n;t;nw;rw;ie;x,mw;lI;g;au;Vw,mw;rU",
 	wordsToDisplay : null,
 	size : 6,
 	rows : 6,
 	cols : 9
 }
-var gridl3v5 = {
+var gridl2v5 = {
 	words : "qu;Kw;rI,ky;dw;rw,BY;r;au,b;sM;qu,sw;r;g,m;lw;r,kw;n;Vw",
 	wordsToDisplay : null,
 	size : 6,
 	rows : 6,
 	cols : 9
 }
-var gridl3v6 = {
+var gridl2v6 = {
 	words : "k;il;Aw;n,pR;Bw;qI,jY;jw;vM;qI,s;lo;k,p;au;VI,m;h;lw,muM;dw;v;xI",
 	wordsToDisplay : null,
 	size : 6,
 	rows : 6,
 	cols : 9
 }
-var gridl3v7 = {
+var gridl2v7 = {
 	words : "s;r;ix,gu;rU,gu;r;du;Aw;rw,sM;g;iq,gu;r;pR;sw;id,su;Aw;mI,ku;r;bw;n",
 	wordsToDisplay : null,
 	size : 6,
 	rows : 6,
 	cols : 9
 }
+var gridl2v8 = {
+	words : "j;pu,jw;pu,s;v;Xy,cO;p;eI,Aw;nM;d,r;ih;rw;s,so;ih;lw",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 9
+}
+var gridl2v9 = {
+	words : "n;gw;Vw,ky;s;kI,bw;tw,A;r;dw;s,du;mw;lw,in;hM;g,bw;xw",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 9
+}
+var gridl2v10 = {
+	words : "qy;rw,kI;qw,jw;qo,nw;hI,mY;no,jo;gu,kI;qo;eI",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 9
+}
+var gridl2v11 = {
+	words : "in;r;gu;ix;Aw;ry,gu;xu,nw;hI,Aw;py,q;r;su,p;ie;E;eI",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 9
+}
+var gridl2v12 = {
+	words : "q;r;su,p;ie;Aw,im;h;rw;m;iq,ho;eI,s;iq;gu;ru,s;j;xu,im;il;Aw",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 11
+}
+var gridl2v13 = {
+	words : "nw;n;k,nw;mu,im;lY,jI;vW,q;nu,m;nu,QI;vY,h;ir;Aw",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 9
+}
+var gridl2v14 = {
+	words : "Qw;l,iv;ic,iqM;in,v;s;qU,p;eI;E,s;qu,sM;qo;Ku,vI;cw;ro",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 9
+}
+var gridl2v15 = {
+	words : "ij;h,is;m;r;q,sM;k;t,im;tY,d;r;su,qu;hw;ro,ho;ie",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 9
+}
+
+var gridl3v1 = {
+	words : "s;iq;nw;mu,k;r;qw;pu;r;Ku,in;r;B;au,in;r;vY;ru,A;kw;l;mU;r;iq,A;jU;nI,sY;BM,gu;r;pR;sw;id",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 11
+}
+var gridl3v2 = {
+	words : "hu;k;im,r;jw;eI,c;l;xw,nw;n;k,il;iK;Aw,nw;il",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v3 = {
+	words : "hu;k;mI,ho;v;in,jI;A,hu;k;im,im;lY,v;if;Aw;eI",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v4 = {
+	words : "hu;k;mI,au;q;mu,nI;cu,hu;k;im,il;iK,du;K,su;K,pw;eI;A;ih",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v5 = {
+	words : "hu;k;mI,hu;k;mu,c;lw;ey,rw;hu,nw;n;k,iv;g;sY,vy;p;r;vw;hu",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v6 = {
+	words : "sw;cw,sw;ih;bu,sw;cu,nw;ie,Bw;iK;Aw,Bw;au,A;pw;r",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v7 = {
+	words : "ij;in,sy;iv;Aw,iq;in,pw;ie;Aw,mw;nu",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
 var gridl3v8 = {
-		words : "j;pu,jw;pu,s;v;Xy,cO;p;eI,Aw;nM;d,r;ih;rw;s,so;ih;lw",
-		wordsToDisplay : null,
-		size : 6,
-		rows : 6,
-		cols : 9
-	}
+	words : "gu;rw,ie;k,dy;ih,bu;Jw;eI",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
 var gridl3v9 = {
-		words : "n;gw;Vw,ky;s;kI,bw;tw,A;r;dw;s,du;mw;lw,in;hM;g,bw;xw",
-		wordsToDisplay : null,
-		size : 6,
-		rows : 6,
-		cols : 9
-	}
+	words : "nw;n;k,in;r;gu;ix,gu;xu,k;ry,gu;x;vM;iq;Aw",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
 var gridl3v10 = {
-		words : "s;iq;nw;mu,k;r;qw;pu;r;Ku,in;r;B;au,in;r;vY;ru,A;kw;l;mU;r;iq,A;jU;nI,sY;BM,gu;r;pR;sw;id",
-		wordsToDisplay : null,
-		size : 6,
-		rows : 6,
-		cols : 11
-	}
+	words : "nw;n;k,B;g;qw,s;dw,iv;gw;su",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v11 = {
+	words : "su;ix;AY,mu;iK,sw;lw;h;x,mM;du",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v12 = {
+	words : "su;ix;AY,s;qu,sM;qo;Ku,ig;Aw;n",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v13 = {
+	words : "su;ix;AY,AM;Dy,pw;v;ih,rw;hu",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v14 = {
+	words : "AY;sw,nw;mu,in;rM;j;nu,ho;ie",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v15 = {
+	words : "mM;nY,su;r;iq,ho;vY,m;in,bu;iD",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v16 = {
+	words : "mM;nY,p;iq,is;au,p;r;g;tu,jw;ie",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v17 = {
+	words : "mM;nY,pw;v;ih,mo;Ku,du;Aw;r",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v18 = {
+	words : "pM;cy,pw;v;ih,d;r;g;ih,mw;n",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v19 = {
+	words : "ku;d;r;iq,k;v;x,k;hw,vI;cw;ru",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v20 = {
+	words : "A;sM;K,A;m;r,k;ir,jw;ih,jo;r",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v21 = {
+	words : "A;sM;K,k;h;ih,is;ir,Bw;ru,ho;ie",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v22 = {
+	words : "nw;n;k,hu;k;mI,Aw;v;hu,jw;h",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v23 = {
+	words : "su;A;s;iq,Aw;iQ,bw;xI,b;r;mw;au",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v24 = {
+	words : "nw;n;k,v;fw,Aw;KI;AY,Aw;py,jw;xY,Aw;pu",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v25 = {
+	words : "s;muM;d,sw;h,su;l;qw;n,ig;r;hw,sy;qI,mw;lu,D;nu",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v26 = {
+	words : "nw;n;k,n;d;rI,k;r;mI,dw;iq",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v27 = {
+	words : "ky;iq;Aw,dU;K,BU;K,s;d,mw;r,ey;ih,dw;iq,qy;rI,dw;qw;r",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v28 = {
+	words : "bM;id,K;lw;sI,Bw;xY,ho;ie",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v29 = {
+	words : "jy;v;fu,Bw;vY,qy;v;fu,ho;ie,nw;n;k,jw;xY,sw;cw,so;ie",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v30 = {
+	words : "gw;v;in,r;q;n,au;pw;ey,qy;ry,A;T;s;iT,qI;r;Q,nw;ly",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v31 = {
+	words : "Aw;eI,pM;QI,s;g;l,j;mw;qI,m;in,jI;qY,j;gu,jI;q",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v32 = {
+	words : "sM;jo;gu,iv;jo;gu,du;ie,kw;r,c;lw;v;ih,ly;Ky,Aw;v;ih,Bw;g",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v33 = {
+	words : "ey;kw,mw;eI,ju;g;iq,iv;Aw;eI,iq;in,cy;ly,p;r;vw;xu",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v34 = {
+	words : "nw;n;k,n;d;rI,pw;eI;AY,kU;VI,kU;VY,TI;s",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v35 = {
+	words : "ij;su,h;iQ,jo;ru,k;ir,vy;KY,so;ie",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v36 = {
+	words : "k;c,p;kw;eI,E;QY,pw;ie,nw;n;k,g;ie;Aw,jw;pY,jw;ie",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v37 = {
+	words : "ky;qI;Aw,Kw;xI,bw;xI,ky;qy,pw;q,n;irM;d",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v38 = {
+	words : "iq;QY,G;VI;AY,su;r;iq,m;iq,m;in,bu;iD",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v39 = {
+	words : "s;c,KM;if,v;sY,in;rM;kw;ru,k;ir,vy;KY,n;d;ir,in;hw;l",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v40 = {
+	words : "G;VI;AY,s;b;du,s;cI,t;k;sw;l",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
+var gridl3v41 = {
+	words : "ij;nI,nw;mu,iD;Aw;ie;Aw,g;ey,m;s;k;iq,Gw;il",
+	wordsToDisplay : null,
+	size : 6,
+	rows : 6,
+	cols : 10
+}
